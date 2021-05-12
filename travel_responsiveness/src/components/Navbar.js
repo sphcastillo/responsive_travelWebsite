45e43cde57cc875cc63b13fa22acf2ce59cae7f2
Sophia Castillo <Sphcastillo@gmail.com>
2021-05-12 14:27:56 -0700

    react-icons are working in JSX in Navbar

--- travel_responsiveness/src/components/Navbar.js
+++ travel_responsiveness/src/components/Navbar.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaTypo3 } from 'react-icons/fa';
+import { FaTypo3, FaBars } from 'react-icons/fa';
+import { IoMdClose } from 'react-icons/io';
 
 
 
 
 
 function Navbar() {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <>
             <nav className="navbar">
@@ -14,8 +22,17 @@ function Navbar() {
                     <Link to="/" className="navbar-logo">
                         TRVL
                         <FaTypo3 />
-
                     </Link>
+                    <div className='menu-icon' onClick={handleClick} >
+                        {click ? <IoMdClose /> : <FaBars /> }
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
