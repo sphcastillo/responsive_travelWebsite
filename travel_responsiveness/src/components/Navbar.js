a2f846059066e7c63994c2937a6a88a051209b1f
Sophia Castillo <Sphcastillo@gmail.com>
2021-05-12 12:57:44 -0700

    images have been added, Navbar has been started, react-icon has been successfully added

--- travel_responsiveness/src/components/Navbar.js
+++ travel_responsiveness/src/components/Navbar.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaTypo3 } from 'react-icons/fa';
+
+
+
+
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        TRVL
+                        <FaTypo3 />
+
+                    </Link>
+                </div>
+            </nav>
+        </>
     )
 }
 
-export default Navbar
+export default Navbar;
