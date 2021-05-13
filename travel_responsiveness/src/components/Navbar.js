018d0ef11ab920311160c98b64b51508329f054c
Sophia Castillo <Sphcastillo@gmail.com>
2021-05-13 11:40:33 -0700

    Navbar.css has been added

--- travel_responsiveness/src/components/Navbar.js
+++ travel_responsiveness/src/components/Navbar.js
@@ -10,10 +10,21 @@ import { IoMdClose } from 'react-icons/io';
 function Navbar() {
 
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
 
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    };
+
+    window.addEventListener("resize", showButton);
 
     return (
         <>
@@ -48,6 +59,7 @@ function Navbar() {
                             </Link>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle="btn--outline">SIGN UP</Button>} 
                 </div>
             </nav>
         </>
